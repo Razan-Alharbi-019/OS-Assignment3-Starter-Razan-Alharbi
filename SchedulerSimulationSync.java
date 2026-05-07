9e97ae04763061a0279fefe73fadd59c90117384
Razan-Alharbi-019 <445052130@std.psau.edu.sa>
2026-05-07 19:27:40 +0300

    Implement locks to protect critical sections
    Added locking mechanisms to critical sections to prevent race conditions.

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -57,26 +57,63 @@ public static final Semaphore cpuSemaphore = new Semaphore(1);
         // TODO: Protect this critical section with a lock
         // RACE CONDITION: Multiple threads might read and write simultaneously!
         
-        contextSwitchCount++;
+        contextSwitchLock.lock();
+
+        try {
+
+            contextSwitchCount++;
+
+        } finally {
+
+            contextSwitchLock.unlock();
+
+        }
     }
     
     // Method to increment completed process counter
     public static void incrementCompletedProcess() {
         // TODO: Protect this critical section with a lock
-        completedProcessCount++;
+        completedProcessLock.lock();
+
+        try {
+
+            completedProcessCount++;
+
+        } finally {
+
+            completedProcessLock.unlock();
+
+        }
     }
     
     // Method to add waiting time
     public static void addWaitingTime(long time) {
         // TODO: Protect this critical section with a lock
-        totalWaitingTime += time;
+        try {
+
+            totalWaitingTime += time;
+
+        } finally {
+
+            waitingTimeLock.unlock();
+
+        }
     }
     
     // Method to log execution
     public static void logExecution(String message) {
         // TODO: Protect this critical section with a lock
         // RACE CONDITION: ArrayList is not thread-safe!
-        executionLog.add(message);
+        logLock.lock();
+
+        try {
+
+            executionLog.add(message);
+
+        } finally {
+logLock.unlock();
+            
+}
     }
 }
 
